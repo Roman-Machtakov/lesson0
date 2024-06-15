7c655fb69843ef467073a3289ea3967827946afa
Roman-Machtakov <79520337274@mail.ru>
2024-06-15 17:25:19 +0300

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,30 @@
+
+#Длинна слов
+a = "Hello, world"
+print(len(a))
+
+#Суммы и разности
+first = 3
+second = 30
+summa = first + second
+diff = second - first
+print(summa, diff)
+
+#Среднее арифметическое
+first = 10
+second = 20
+third = 30
+mean = (first + second + third) / 3
+print(mean)
+
+#Простые строчки
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string +', '+ first_string)
+
+#Сложная формула
+a = 3
+b = 4
+c = 5
+f = ((a*b) + (a*c)) ** 3 / 2
+print(f)
